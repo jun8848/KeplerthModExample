4509d0a5752871df7f673955d6942a4321cd817e
mubai <2214792487@qq.com>
2022-09-21 03:25:28 +0800

    添加方块

---
+++ KeplerthModExample/CreateNewPlacement.cs
@@ -0,0 +1,49 @@
+﻿using DataBase;
+using Harmony;
+using Keplerth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KeplerthModExample
+{
+    [HarmonyPatch(typeof(ConfigPlacement), "initData")]
+    public class CreateNewPlacement
+    {
+        public static bool Prefix()
+        {
+
+
+            ConfigPlacement.Table.Clear();
+            WWW www = new WWW(Config.filepath + "Config/ConfigPlacement.json");
+            while (!www.isDone)
+            {
+            }
+            ConfigPlacement.Table = JsonConvert.DeserializeObject<Dictionary<int, ConfigPlacement>>(www.text);
+            www.Dispose();
+            CreatePlacement();
+
+            Workshop.LoadModsConfig<ConfigPlacement>(ConfigPlacement.Table, "ConfigPlacement");
+            return false;
+        }
+
+        public static void CreatePlacement()
+        {
+            int id = 200000;
+
+            for (int i = 0; i < 10; i++)
+            {
+                ConfigPlacement data = ConfigPlacement.Table[2001];
+                ConfigPlacement placement = new ConfigPlacement(data);
+                placement.HandItemId = id + i;
+                ModDebug.Log(placement.HandItemId);
+                ConfigPlacement.Table.Add(id + 10000 + i, placement);
+            }
+            
+        }
+    }
+}
--- KeplerthModExample/ModDebug.cs
+++ KeplerthModExample/ModDebug.cs
@@ -4,20 +4,20 @@ using System.Text;
 
 namespace KeplerthModExample
 {
-    //public class ModDebug
-    //{
-        
-    //    public static void Log(object msg)
-    //    {
-    //        string text = "C:\\Users\\mubai\\Desktop\\Log\\";
-    //        bool flag = !Directory.Exists(text);
-    //        if (flag)
-    //        {
-    //            Directory.CreateDirectory(text);
-    //        }
-    //        StreamWriter streamWriter = new StreamWriter(text + "itemLog.txt", true, Encoding.UTF8);
-    //        streamWriter.WriteLine(DateTime.Now.ToString() + ":\t" + msg.ToString());
-    //        streamWriter.Close();
-    //    }
-    //}
+    public class ModDebug
+    {
+
+        public static void Log(object msg)
+        {
+            string text = "C:\\Users\\mubai\\Desktop\\Log\\";
+            bool flag = !Directory.Exists(text);
+            if (flag)
+            {
+                Directory.CreateDirectory(text);
+            }
+            StreamWriter streamWriter = new StreamWriter(text + "itemLog.txt", true, Encoding.UTF8);
+            streamWriter.WriteLine(DateTime.Now.ToString() + ":\t" + msg.ToString());
+            streamWriter.Close();
+        }
+    }
 }
